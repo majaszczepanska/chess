a52cb054f1b7fad9726900056e6c29204e5d2476
Maja Szczepańska <maja.szczepanska06@gmail.com>
2025-12-04 16:45:46 +0100

    add pawn promotion, change highlights, add description

--- game/src/main/java/com/chess/Main.java
+++ game/src/main/java/com/chess/Main.java
@@ -15,15 +15,10 @@ public class Main {
 
 
 /*lack of function:
-- castling (king and rook)
-- exchange pawn for a different piece
 - pawn hop
-- moving options
-- killing options
 - last move (colors)
-- unclick own piece 
 - restart game
 - prettier message (win)
-- checkmate (first move then message)
 - view on killed pieces
+- delate click on null squares
 */
--- game/src/main/java/com/chess/view/GameWindow.java
+++ game/src/main/java/com/chess/view/GameWindow.java
@@ -5,9 +5,9 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Insets;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import com.chess.model.Board;
 import com.chess.model.Piece;
@@ -15,26 +15,37 @@ import com.chess.model.Piece;
 public class GameWindow extends JFrame {
 
     private final JButton[][] squares = new JButton[8][8];
-    private final Board board;
+    private Board board;
+    private JPanel sidePanel;
+    private JButton restartButton;
     private Piece selectedPiece = null;
     private boolean isWhiteTurn = true;
     private int selectedX = -1;
     private int selectedY = -1;
     private Color currentColor = null;
 
-    boolean isWhiteCastlingPossible = true;
-    boolean isBlackCastlingPossible = true;
+    //LAST MOVE
+    private int lastMoveStartX = -1;
+    private int lastMoveStartY = -1;
+    private int lastMoveEndX = -1;
+    private int lastMoveEndY = -1;
 
 
     //COLORS
     //board
     private final Color lightSquare = new Color(209, 156, 155);
     private final Color darkSquare = new Color(145, 95, 109);
-    private final Color background = new Color(153, 201, 94);
-    private final Color border = new Color(102, 68, 89);
+    //piece
+    private final Color clickedPieceBackground = new Color(153, 201, 94);
+    private final Color background = new Color(102, 68, 89);
+    
     //move
-    private final Color possibleMove = new Color(142, 173, 134);
+    private final Color possibleMove = new Color(148, 255, 177);
+    //private final Color possibleMoveBorder = new Color(61, 105, 67);
     private final Color possibleCapture = new Color(191, 61, 61);
+    //private final Color possibleCaptureBorder = new Color(122, 20, 15);
+    //private final Color lastMove = new Color(220, 185, 60);
+    //private final Color lastMoveBorder = new Color(158, 84, 13);
 
     public GameWindow() {
         setTitle("Chess Game - White starts");
@@ -45,12 +56,12 @@ public class GameWindow extends JFrame {
         //create new board
         board = new Board();
         drawBoard();
-        getContentPane().setBackground(border);
+        getContentPane().setBackground(background);
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
-    //paint the board
+    //PAIN THE BOARD
     private void drawBoard() {   
         for (int y=0; y<8; y++){
             for (int x=0; x<8; x++){
@@ -74,12 +85,11 @@ public class GameWindow extends JFrame {
                 //add button to the frame
                 add(squares[y][x]);
             }
-            
         }
         refreshBoard();
     }
 
-    //update the board display add pieces
+    //UPDATE THE BOARD - ADD PIECES
     private void refreshBoard() {
         for (int y=0; y<8; y++){
             for (int x=0; x<8; x++){
@@ -95,11 +105,12 @@ public class GameWindow extends JFrame {
     }
 
 
-    //button clicked
+    //BUTTON CLICKED
     private void handleClick(int x, int y){
-        //first click
+        //1. FIRST CLICK
         if (selectedPiece == null) {
             Piece piece = board.getPiece(x, y);
+
             //if there is a piece and it's the correct turn
             if(piece != null && piece.isWhite() == isWhiteTurn) {
                 //selected parameters
@@ -107,16 +118,16 @@ public class GameWindow extends JFrame {
                 selectedX = x;
                 selectedY = y;
                 currentColor = squares[selectedY][selectedX].getBackground();
-                squares[selectedY][selectedX].setBackground(background);
+                squares[selectedY][selectedX].setBackground(clickedPieceBackground);
                 showMoves();
             }
         }
 
 
 
-        //selectedPiece is not null - second click
+        //2. SECOND CLICK - selectedPiece is not null 
         else {
-            //deselect if clicked the same square
+            //1. THE SAME SQUARE - deselect 
             if (x == selectedX && y == selectedY) {
                 resetSelection();
                 return;
@@ -125,25 +136,23 @@ public class GameWindow extends JFrame {
             //move selected piece to selected target square
             int targetX = x;
             int targetY = y;
-            //get target piece/square
+            //get target square
             Piece target = board.getPiece(targetX, targetY);
 
-            //function to prevent extra clicking when changing one's mind when selecting which piece should move
-            //if second click is click in piece in the same color
+            //2. SECOND CLICK IS PIECE IN THE SAME COLOR AS FIRST
             if (target != null && target.isWhite() == isWhiteTurn){
-                //reset color of the piece from first click, do the same thing as in the first click  
+                //reset color of the square from first click, do the same thing as in the first click  
                 resetSelection();  
                 selectedPiece = target;
                 selectedX = targetX;
                 selectedY = targetY;
                 currentColor = squares[targetY][targetX].getBackground();
-                squares[targetY][targetX].setBackground(background);
+                squares[targetY][targetX].setBackground(clickedPieceBackground);
                 showMoves();
                 return;
-                
             }
             
-            //can not capture the king
+            //3. RESET SELECTION IF USER WANT TO CAPTURE KING
             if (target != null) {
                 String pieceType = target.getClass().getSimpleName();
                 if (pieceType.equals("King")){
@@ -152,22 +161,28 @@ public class GameWindow extends JFrame {
   
             }
 
-            //check if the move is valid
+            //4. MOVE IS VALID
             if (selectedPiece.isValidMove(selectedX, selectedY, targetX, targetY, board)) {
-                //simulate move to check for check condition
+                //for show last move
+                lastMoveStartX = selectedX;
+                lastMoveStartY = selectedY;
+                lastMoveEndX = targetX;
+                lastMoveEndY = targetY;
                 //put selected piece to target square and remove from original square
                 board.setPiece(targetX, targetY, selectedPiece);
                 board.setPiece(selectedX, selectedY, null);
+                //if isCheck - undo move
                 if (board.isCheck(isWhiteTurn)) {
                     board.setPiece(selectedX, selectedY, selectedPiece);
                     board.setPiece(targetX, targetY, target);
                     return;
                 }
 
+                //get name of selected piece
                 String pieceType = selectedPiece.getClass().getSimpleName();
             
                 
-                //King moved for 2 squares - castling
+                //1. CASTLING - king moved 2 squares
                 if (pieceType.equals("King") && Math.abs(targetX-selectedX) == 2){
                     //white castling - short
                     if (targetX == 6 && targetY == 7){
@@ -202,15 +217,18 @@ public class GameWindow extends JFrame {
                 //important to set true to hasMoved in selectedPiece
                 selectedPiece.setHasMoved(true);
 
-                //pawn exchange
+                //2. PAWN EXCHANGE
                 boolean currentColor = selectedPiece.isWhite();
                 refreshBoard();
                 resetSelection();
                 if (pieceType.equals("Pawn") && (targetY == 0 || targetY == 7)){
+                    //option in exchange
                     String[] options = {"Queen", "Rook", "Bishop", "Knight"};
+                    //show an option box
                     int choice = javax.swing.JOptionPane.showOptionDialog(this, "Select piece to receive for pawn: ", "Promotion",
                         javax.swing.JOptionPane.DEFAULT_OPTION, javax.swing.JOptionPane.QUESTION_MESSAGE, null, options, options[0]
                     );
+                    //create new Piece based on choice
                     Piece promotedPiece;
                     switch(choice){
                         case 1:
@@ -229,19 +247,23 @@ public class GameWindow extends JFrame {
                     board.setPiece(targetX, targetY, promotedPiece);
                     refreshBoard();
                 }
+
+                
+                
                 nextTurn();
             }
         }
     }
 
-
+    //ACTION AFTER MOVE ENDS
     private void nextTurn() {
         //change turn
         isWhiteTurn = !isWhiteTurn;
         boolean inCheck = board.isCheck(isWhiteTurn);
         boolean canMove = board.hasAnyMove(isWhiteTurn);
-        //if king in check
+        //KING CHECKED
         if (inCheck){
+            //CHECKMATE
             if (!canMove) {
                 setTitle("Checkmate, " +(!isWhiteTurn ? "White" : "Black")+" won!");
                 javax.swing.JOptionPane.showMessageDialog(this, "Checkmate\nEnd of the game, " +(!isWhiteTurn ? "White" : "Black")+" won!");
@@ -250,6 +272,7 @@ public class GameWindow extends JFrame {
                 setTitle("Chess - Turn: " +(isWhiteTurn ? "White" : "Black"));
             }
         } else {
+            //STALEMATE
             if (!canMove){
                 setTitle("Stalemate, nobody won!");
                 javax.swing.JOptionPane.showMessageDialog(this, "Stalemate\nEnd of the game, withdraw");
@@ -261,7 +284,7 @@ public class GameWindow extends JFrame {
     }
 
 
-
+    //SHOW POSSIBLE MOVES
     private void showMoves(){
         for (int y = 0; y < 8; y++) {
             for (int x = 0; x < 8; x++) {
@@ -270,24 +293,24 @@ public class GameWindow extends JFrame {
                     if (x == selectedX && y == selectedY){
                         continue;
                     }
-                    if (target != null && target.isWhite() == isWhiteTurn){
+                    if (target != null && target.isWhite() == selectedPiece.isWhite()){
                         continue;
                     }
 
+                    //LOOK FOR MOVES - PREVENT CHECK
                     board.setPiece(x, y, selectedPiece);
                     board.setPiece(selectedX, selectedY, null);
                     boolean moveSafe = !board.isCheck(isWhiteTurn);
                     board.setPiece(selectedX, selectedY, selectedPiece);
                     board.setPiece(x, y, target);
+                    //MOVE IS SAFE
                     if (moveSafe){
-                        squares[y][x].setBackground(possibleMove);
-                        squares[y][x].setBorder(BorderFactory.createCompoundBorder(
-                            BorderFactory.createEmptyBorder(1,1,1,1),
-                            BorderFactory.createLineBorder(border, 3)
-                        ));
-                        squares[y][x].setBorderPainted(true);
-                        if (target != null){
-                             squares[y][x].setBackground(possibleCapture);
+                        if (target == null){
+                            squares[y][x].setForeground(possibleMove);
+                            squares[y][x].setText("•");
+                        } else {
+                            squares[y][x].setBackground(possibleCapture);
+                            //paintBorder(y, x, possibleCaptureBorder);
                         }
                     }
                 }
@@ -298,7 +321,7 @@ public class GameWindow extends JFrame {
     
 
 
-    //reset selection - clear selected piece and restore square color
+    //RESET SELECTION - clear selected piece and restore square color
     private void resetSelection(){
         squares[selectedY][selectedX].setBackground(currentColor);
         selectedPiece = null;
@@ -314,12 +337,27 @@ public class GameWindow extends JFrame {
                 squares[y][x].setBorderPainted(false);
                 squares[y][x].setBorder(null);
             }
+            refreshBoard();
         }
-                
+        //show last move in different color
+        /*
+        if (lastMoveStartX != -1) {
+            squares[lastMoveStartY][lastMoveStartX].setBackground(lastMove);
+            squares[lastMoveEndY][lastMoveEndX].setBackground(lastMove);
+
+        }
+        */      
     }
-        
- 
+    
+    /* 
+    private void paintBorder(int y, int x, Color color){
+        squares[y][x].setBorder(BorderFactory.createCompoundBorder(
+            BorderFactory.createEmptyBorder(1,1,1,1),
+            BorderFactory.createLineBorder(color, 3)
+        ));
+        squares[y][x].setBorderPainted(true);
+    }
 
-
+    */
     
 }
